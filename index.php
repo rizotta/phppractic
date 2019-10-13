9ef8c2c669a5ee5ccdaf33a90c6fb65106b7bf03
Svetlana <svetlananepoklonova@gmail.com>
2019-10-13 23:57:43 +0300

    Added link to 6 folder

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	<meta charset="UTF-8">
 	<title>Практика PHP</title>
 	<link rel="stylesheet" href="style.css">
-	<script src="main.js"></script>
 </head>
 <body>
 	<h3>Welcome. Практика работы с PHP.</h3>
@@ -13,7 +12,7 @@
 		<li><a href="3/answerForHWLesson3.php">3. Циклы, массивы. Задания</a></li>
 		<li><a href="4/admin/">4. Работа с файлами: загрузка, отображение на странице</a></li>
 		<li><a href="5/">5. БД MySQL</a></li>
-	
+		<li><a href="6/">6. Интерактивность. Онлайн-чат в связке с БД</a></li>
 	</ul>
 </body>
 </html>
